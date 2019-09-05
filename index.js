7a246b518920486e4c1ad552da52b25e92136831
Faye Skeen <fskeen@gmail.com>
2019-09-05 13:16:44 -0700

    no env port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 require('dotenv').config()
 const server = require('./server')
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 server.listen(8000, () => console.log(`Server listening at port ${port}. Rock on!`))
