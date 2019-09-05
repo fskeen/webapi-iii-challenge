6afc40faa193527336564cb8407f6589e365f0af
Faye Skeen <fskeen@gmail.com>
2019-09-05 13:04:58 -0700

    Making ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
+require('dotenv').config()
 const server = require('./server')
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(8000, () => console.log(`Server listening at port ${port}. Rock on!`))
